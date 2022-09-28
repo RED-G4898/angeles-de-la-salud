77db9e77100793f1701cc006d073382f764a1c92
Rodrigo Delgadillo <red_g4898@proton.me>
2022-09-28 01:34:13 +0000

    Add commets to the code and make some variable, constant and function name changes.

--- js/productos.js
+++ js/productos.js
@@ -1,76 +1,98 @@
-// Variables
+/*
+ * The following code is a preliminary version of a shopping price comparator algorithm,
+ * It is intended to show to users the price of several products in Farmacias Ángeles de la Salud
+ * and the competition, and also to show the total price of the products selected by the user and the
+ * its savings if it chooses to buy in our pharmacy.
+ *
+ * The whole code has been written and developed by RED with <3 for Farmacias Ángeles de la Salud as
+ * part of a project from a Coderhouse course.
+ */
 
-// Productos
-const paracetamol1 = 12;
-const paracetamol2 = 30;
-const amlodipino1 = 30;
-const amlodipino2 = 45;
-const omeprazol1 = 105;
-const omeprazol2 = 158;
-const amoxicilina1 = 39;
-const amoxicilina2 = 45;
+// FAS Stand for Farmacias Ángeles de la Salud.
 
-// Otros
-let total1 = 0;
-let total2 = 0;
-let opt1;
-let opt2;
+// Constant declaration and initialization
+
+// Products
+const FAS_PARACETAMOL_PRICE = 12;
+const COMPETITION_PARACETAMOL_PRICE = 30;
+
+const FAS_AMLODIPINE_PRICE = 30;
+const COMPETITION_AMLODIPINE_PRICE = 45;
+
+const FAS_OMEPRAZOLE_PRICE = 105;
+const COMPETITION_OMEPRAZOLE_PRICE = 158;
+
+const FAS_AMOXICILLIN_PRICE = 39;
+const COMPETITION_AMOXICILLIN_PRICE = 45;
+
+// Variables declaration and initialization
+
+// Control variables
+let fasTotal = 0;
+let competitionTotal = 0;
+let productsOpt;
+let menuOpt;
+
+// Main code
 
 alert("Bienvenido al comparador de precios de Farmacias Ángeles de la Salud");
 do {
-    if (opt1 == undefined){
-        opt1 = prompt("¿Qué producto desea comparar? (Ingrese uno de los siguientes números)\n1 - Paracetamol\n2 - Amlodipino\n3 - Omeprazol\n4 - Amoxicilina");
+    if (productsOpt == undefined){
+        productsOpt = prompt("¿Qué producto desea comparar? (Ingrese uno de los siguientes números)\n1 - Paracetamol\n2 - Amlodipino\n3 - Omeprazol\n4 - Amoxicilina");
     }
-    switch (opt1) {
+    switch (productsOpt) {
         case "1":
-            total1+=paracetamol1;
-            total2+=paracetamol2;
-            mostrarProductos("Paracetamol", paracetamol1, paracetamol2);
-            mostrarTotal();
+            fasTotal+=FAS_PARACETAMOL_PRICE;
+            competitionTotal+=COMPETITION_PARACETAMOL_PRICE;
+            showProducts("Paracetamol", FAS_PARACETAMOL_PRICE, COMPETITION_PARACETAMOL_PRICE);
+            showTotalPrice();
             break;
         case "2":
-            total1+=amlodipino1;
-            total2+=amlodipino2;
-            mostrarProductos("Amlodipino", amlodipino1, amlodipino2);
-            mostrarTotal();
+            fasTotal+=FAS_AMLODIPINE_PRICE;
+            competitionTotal+=COMPETITION_AMLODIPINE_PRICE;
+            showProducts("Amlodipino", FAS_AMLODIPINE_PRICE, COMPETITION_AMLODIPINE_PRICE);
+            showTotalPrice();
             break;
         case "3":
-            total1+=omeprazol1;
-            total2+=omeprazol2;
-            mostrarProductos("Omeprazol", omeprazol1, omeprazol2);
-            mostrarTotal();
+            fasTotal+=FAS_OMEPRAZOLE_PRICE;
+            competitionTotal+=COMPETITION_OMEPRAZOLE_PRICE;
+            showProducts("Omeprazol", FAS_OMEPRAZOLE_PRICE, COMPETITION_OMEPRAZOLE_PRICE);
+            showTotalPrice();
             break;
         case "4":
-            total1+=amoxicilina1;
-            total2+=amoxicilina2;
-            mostrarProductos("Amoxicilina", amoxicilina1, amoxicilina2);
-            mostrarTotal();
+            fasTotal+=FAS_AMOXICILLIN_PRICE;
+            competitionTotal+=COMPETITION_AMOXICILLIN_PRICE;
+            showProducts("Amoxicilina", FAS_AMOXICILLIN_PRICE, COMPETITION_AMOXICILLIN_PRICE);
+            showTotalPrice();
             break;
         default:
             alert("Opción inválida");
             break;
     }
-    opt2 = prompt("¿Desea comparar otro producto? (si/no)");
+    menuOpt = prompt("¿Desea comparar otro producto? (si/no)");
     do {
-        if (opt2 === "si"){
-            opt1 = prompt("¿Qué otro producto desea comparar? (Ingrese uno de los siguientes números)\n1 - Paracetamol\n2 - Amlodipino\n3 - Omeprazol\n4 - Amoxicilina");
-        } else if (opt2 != "si" && opt2 != "no"){
-            opt2 = prompt("Opción inválida, ¿desea comparar otro producto? (si/no)");
+        if (menuOpt === "si"){
+            productsOpt = prompt("¿Qué otro producto desea comparar? (Ingrese uno de los siguientes números)\n1 - Paracetamol\n2 - Amlodipino\n3 - Omeprazol\n4 - Amoxicilina");
+        } else if (menuOpt != "si" && menuOpt != "no"){
+            menuOpt = prompt("Opción inválida, ¿desea comparar otro producto? (si/no)");
         }
-    } while (opt2 != "si" && opt2 != "no");
-} while (opt2 != "no");
+    } while (menuOpt != "si" && menuOpt != "no");
+} while (menuOpt != "no");
 
-mostrarTotal();
+showTotalPrice();
 
+// EOF
 
-function mostrarProductos(nombre, precio1, precio2){
-    alert("El precio de " + nombre + " en Farmacias Ángeles de la Salud es de: $" + precio1 + " mxn" + "\n" + "El precio de " + nombre + " en la competencia es de: $" + precio2 + " mxn");
+// Function definitions
+
+function showProducts(name, price1, price2){
+    alert("El precio de " + name + " en Farmacias Ángeles de la Salud es de: $" + price1 + " mxn" + "\n" + "El precio de " + name + " en la competencia es de: $" + price2 + " mxn");
 }
 
-function mostrarTotal(){
-    if (total1 > 0 && total2 > 0){
-        alert("Si compra con nosotros usted pagará un total de: $" + total1 + " mxn" + "\n" + "Si compra con la competencia usted pagará un total de: $" + total2 + " mxn" + "\n" + "Usted ahorrará: $" + (total2 - total1) + " mxn");
-        if (opt2 === "no"){
+function showTotalPrice(){
+    if (fasTotal > 0 && competitionTotal > 0){
+        alert("Si compra con nosotros usted pagará un total de: $" + fasTotal + " mxn" + "\n" + "Si compra con la competencia usted pagará un total de: $" + competitionTotal + " mxn" + "\n" + "Usted ahorrará: $" + (competitionTotal - fasTotal) + " mxn");
+        if (menuOpt === "no"){
             alert("Gracias por utilizar nuestro comparador de precios");
         }
     }
